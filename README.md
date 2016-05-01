0d45d366429eaa90415a0763f5276b0cb2f8d401
Khalilur Rahman <khal.rahman@gmail.com>
2016-05-01 12:06:49 +0800

    added live-server

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # React Hack
+---
+
+**Run the project**
+
+* Install node packages
+```bash
+$ npm install
+```
+
+* Run application
+```bash
+$ npm start
+```
--- components/comment/comment-box.jsx
+++ components/comment/comment-box.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//noinspection JSUnresolvedVariable
 import CommentList from './comment-list.jsx';
-//noinspection JSUnresolvedVariable
 import CommentForm from './comment-form.jsx';
 
 class CommentBox extends React.Component {
